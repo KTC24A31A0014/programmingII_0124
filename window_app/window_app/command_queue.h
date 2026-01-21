d686f866f35be2a934775b3bec87322f968c9527
ktc24a31a0014 <ktc24a31a0014@edu.kyoto-tech.ac.jp>
2026-01-22 04:31:09 +0900

    コミットし忘れ

---
+++ window_app/window_app/command_queue.h
@@ -0,0 +1,19 @@
+//コマンドキュー制御クラス
+#pragma once
+
+#include "device.h"
+
+class CommandQueue final
+{
+	public:
+		CommandQueue() = default;
+		~CommandQueue() = default;
+
+		[[nodiscard]] bool create() noexcept;
+
+		[[nodiscard]] ID3D12CommandQueue* get() const noexcept;
+
+	private:
+		Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue_{}; // コマンドキュー
+};
+
